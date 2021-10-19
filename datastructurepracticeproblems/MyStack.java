f29e232d47400db3e2da7111cca7190aa8d25a1a
priyanka897-star <priyankamore8997@gmail.com>
2021-10-19 16:52:08 +0530

    Added:stack and queue problems

---
+++ datastructurepracticeproblems/MyStack.java
@@ -0,0 +1,34 @@
+package datastructurepracticeproblems;
+
+public class MyStack<K> {
+	
+	private final MyNode<K> myNode;
+	
+	public MyStack() {
+		myNode = new MyNode<>();
+	}
+	
+	public void push (INode<K> element) {
+		myNode.add(element);
+		}
+	public INode peak(){
+		return myNode.head;
+		}
+	
+	
+	public INode pop(){
+		return myNode.pop();
+		}
+	
+	public void printStack() {
+		myNode.printNodes();
+	}
+
+	
+	
+	
+	
+	
+
+
+}
---
+++ datastructurepracticeproblems/MyStackTest.java
@@ -0,0 +1,22 @@
+package datastructurepracticeproblems;
+
+public class MyStackTest {
+	
+	public static void main(String[] args) {
+		givenNumberWhenAddedToStackShouldHaveLastAddedNode();
+	}
+	public  static void givenNumberWhenAddedToStackShouldHaveLastAddedNode() {
+		MyStack<Integer> myStack = new MyStack<Integer>();
+		Node<Integer> firstNumberNode = new Node<Integer>(70);
+		Node<Integer> secondNumberNode = new Node<Integer>(30);
+		Node<Integer> thirdNumberNode = new Node<Integer>(56);
+		myStack.push(firstNumberNode);
+		myStack.push(secondNumberNode);
+		myStack.push(thirdNumberNode);
+		INode myNode =  myStack.peak();
+		myStack.printStack();
+		boolean result = myStack.peak().equals(thirdNumberNode);
+	    System.out.println(result);
+		}
+
+}
