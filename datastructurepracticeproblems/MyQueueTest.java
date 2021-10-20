99f0bcca28504e7cf8e5b30dfabb9d586d1e84dd
priyanka897-star <83417119+priyanka897-star@users.noreply.github.com>
2021-10-20 22:45:33 +0530

    Refactored:Indentation Correction

--- datastructurepracticeproblems/MyQueueTest.java
+++ datastructurepracticeproblems/MyQueueTest.java
@@ -1,23 +1,27 @@
 package datastructurepracticeproblems;
 
+import linkedlistpracticeproblems.INode;
+import linkedlistpracticeproblems.Node;
+
 public class MyQueueTest {
 	public static void main(String[] args) {
-		givenNumberWhenPOPShouldMatchWithLastAddedNode();
+		givenNumberInQueueWhenPOPShouldMatchWithLastAddedNode();
 	}
-	public  static void givenNumberWhenPOPShouldMatchWithLastAddedNode() {
-		MyQueue<Integer> myQueue = new MyQueue<Integer>();
-		Node<Integer> firstNumberNode = new Node<Integer>(70);
-		Node<Integer> secondNumberNode = new Node<Integer>(30);
-		Node<Integer> thirdNumberNode = new Node<Integer>(56);
+	public  static void givenNumberInQueueWhenPOPShouldMatchWithLastAddedNode() {
+		MyQueue myQueue = new MyQueue();
+		Node <Integer>firstNumberNode = new Node<Integer>(56);
+		Node <Integer> secondNumberNode = new Node<Integer>(30);
+		Node<Integer> thirdNumberNode = new Node<Integer>(70);
 		myQueue.enqueue(firstNumberNode);
 		myQueue.enqueue(secondNumberNode);
 		myQueue.enqueue(thirdNumberNode);
-		INode pop =  myQueue.dequeue();
-		myQueue.printStack();
-	    if( !firstNumberNode.isEmpty());
-	    {
-	   System.out.println(true);
-	  }
+		myQueue.printQueue();
+		INode peak =  myQueue.peak();
+		INode dequeue =  myQueue.dequeue();
+		myQueue.printQueue();
+	        boolean result = thirdNumberNode.equals(dequeue ) && thirdNumberNode != peak ;
+	       System.out.println(result);
+    }
 }
-}
+
 
